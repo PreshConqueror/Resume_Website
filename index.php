dfe516faa547aa05536b8c97900bbf823f95fdf1
conqueror-74 <chosendestiny3@gmail.com>
2021-08-22 02:01:26 +0200

    commit

--- index.php
+++ index.php
@@ -3,3 +3,20 @@
 include_once("index.html");
 
 ?>
+
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("pconqueror07@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
